8661a5cf986bbbf4d95dc83fa0d1a5a972f7d067
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2024-02-11 19:18:29 -0300

    Include the annotation to getter, setters, and constructors

--- src/main/java/com/middlewaredev/certification_nlw/modules/students/entities/AnswersCertificationsEntity.java
+++ src/main/java/com/middlewaredev/certification_nlw/modules/students/entities/AnswersCertificationsEntity.java
@@ -2,6 +2,13 @@ package com.middlewaredev.certification_nlw.modules.students.entities;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnswersCertificationsEntity {
     
     private UUID id;
--- src/main/java/com/middlewaredev/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/middlewaredev/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,8 +1,17 @@
 package com.middlewaredev.certification_nlw.modules.students.entities;
 
 import java.util.UUID;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentEntity {
     private UUID id;
-
+    private String email;
+    private List<CestificationStudentEntity> certificationStudentEntity;
 }
