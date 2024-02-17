b7056565afc59bbf1d3f2eab370bf20592cf6990
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2024-02-17 16:41:50 -0300

    Correct the entity map

--- src/main/java/com/middlewaredev/certification_nlw/modules/questions/QuestionEntity.java
+++ src/main/java/com/middlewaredev/certification_nlw/modules/questions/QuestionEntity.java
@@ -2,15 +2,17 @@ package com.middlewaredev.certification_nlw.modules.questions;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.mapping.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,6 +32,8 @@ public class QuestionEntity {
 
     private String description;
 
+    @OneToMany
+    @JoinColumn(name = "question_id")
     private List<AlternativesEntity> alternatives;
 
     @CreationTimestamp
